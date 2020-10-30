25efee6b3a22373ee196b9c9d69b5aaad02eac6d
Simon Mathis (WSL Ubuntu 18.04) <simon.mathis@gmail.com>
2020-10-30 01:14:52 +0000

    formatting

--- {{cookiecutter.repository_name}}/src/tests/test_data.py
+++ {{cookiecutter.repository_name}}/src/tests/test_data.py
@@ -2,9 +2,8 @@ import unittest
 
 
 class TestCase(unittest.TestCase):
-
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual("foo".upper(), "FOO")
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestCase)
